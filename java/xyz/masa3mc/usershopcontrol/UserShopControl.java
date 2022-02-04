6d4e5b54e98ea5dfd06331f4b8d7615c225aad0b
なめたろう <68547657+Name-kun@users.noreply.github.com>
2022-02-04 15:57:31 +0900

    Update UserShopControl.java

--- java/xyz/masa3mc/usershopcontrol/UserShopControl.java
+++ java/xyz/masa3mc/usershopcontrol/UserShopControl.java
@@ -7,20 +7,32 @@ import com.sk89q.worldguard.domains.DefaultDomain;
 import com.sk89q.worldguard.protection.managers.RegionManager;
 import com.sk89q.worldguard.protection.regions.ProtectedRegion;
 import com.sk89q.worldguard.protection.regions.RegionContainer;
+import net.md_5.bungee.api.ChatMessageType;
+import net.md_5.bungee.api.chat.TextComponent;
 import net.milkbowl.vault.economy.Economy;
+import net.raidstone.wgevents.events.RegionEnteredEvent;
+import org.bukkit.Bukkit;
 import org.bukkit.Material;
+import org.bukkit.block.Block;
 import org.bukkit.block.Sign;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.ConsoleCommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.event.player.PlayerMoveEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.potion.PotionEffectType;
 
+import java.util.HashMap;
 import java.util.Objects;
 import java.util.regex.Pattern;
 
-public final class UserShopControl extends JavaPlugin implements Listener {
+public final class UserShopControl extends JavaPlugin implements Listener, CommandExecutor {
 
     private static Economy econ = null;
 
@@ -29,6 +41,7 @@ public final class UserShopControl extends JavaPlugin implements Listener {
         if (!setupEconomy()) {
             getLogger().info("§c経済プラグインが導入されていないため無効化しました。");
             getServer().getPluginManager().disablePlugin(this);
+            getCommand("shopowner").setExecutor(this);
             return;
         }
         getServer().getPluginManager().registerEvents(this, this);
@@ -50,6 +63,9 @@ public final class UserShopControl extends JavaPlugin implements Listener {
         return econ;
     }
 
+    private final HashMap<Player, Boolean> checkConfirm = new HashMap<>();
+    private final HashMap<Player, Block> clickedBlock = new HashMap<>();
+
     @EventHandler
     public void onSignClick(PlayerInteractEvent e) {
         if (e.getPlayer().getWorld().getName().equalsIgnoreCase("shop")) {
@@ -68,34 +84,83 @@ public final class UserShopControl extends JavaPlugin implements Listener {
                         } else if (regions.getRegionCountOfPlayer(WorldGuardPlugin.inst().wrapPlayer(e.getPlayer())) >= 2) { //既に２区画所有している場合
                             e.getPlayer().sendMessage("§c3区画以上は購入できません。");
                         } else {
-                            Pattern outside = Pattern.compile("^[efghijklmnop].*[0-9]$");
-                            Pattern inside = Pattern.compile("^[abcd].*[0-9]$");
-                            Economy economy = getEconomy();
-                            //外側区画の保護名と一致する場合
-                            if (outside.matcher(region.getId()).matches()) {
-                                if (economy.getBalance(e.getPlayer()) > 50000) {
-                                    economy.withdrawPlayer(e.getPlayer(), 50000);
-                                    DefaultDomain defaultDomain = region.getOwners();
-                                    defaultDomain.addPlayer(WorldGuardPlugin.inst().wrapPlayer(e.getPlayer()));
-                                    region.setOwners(defaultDomain);
-                                    e.getPlayer().sendMessage("§6" + region.getId() + "§aを購入しました！");
-                                } else {
-                                    e.getPlayer().sendMessage("§c必要金額は50000円ですが、所持金額が足りません。");
-                                }
-                            } else if (inside.matcher(region.getId()).matches()) { //内側区画の保護名と一致する場合
-                                if (economy.getBalance(e.getPlayer()) > 100000) {
-                                    economy.withdrawPlayer(e.getPlayer(), 100000);
-                                    DefaultDomain defaultDomain = region.getOwners();
-                                    defaultDomain.addPlayer(WorldGuardPlugin.inst().wrapPlayer(e.getPlayer()));
-                                    region.setOwners(defaultDomain);
-                                    e.getPlayer().sendMessage("§6" + region.getId() + "§aを購入しました！");
-                                } else {
-                                    e.getPlayer().sendMessage("§c必要金額は100000円ですが、所持金額が足りません。");
+                            //初めてクリックしたとき
+                            if (checkConfirm.get(e.getPlayer()) == null || !checkConfirm.get(e.getPlayer())) {
+                                checkConfirm.put(e.getPlayer(), true);
+                                clickedBlock.put(e.getPlayer(), e.getClickedBlock());
+                                TextComponent message = new TextComponent();
+                                message.setText("§e§l看板を再度クリックして購入");
+                                e.getPlayer().spigot().sendMessage(ChatMessageType.ACTION_BAR, message);
+                            } else {
+                                Pattern outside = Pattern.compile("^[efghijklmnop].*[0-9]$");
+                                Pattern inside = Pattern.compile("^[abcd].*[0-9]$");
+                                Economy economy = getEconomy();
+                                //外側区画の保護名と一致する場合
+                                if (outside.matcher(region.getId()).matches()) {
+                                    if (economy.getBalance(e.getPlayer()) >= 50000) {
+                                        regionPurchase(e.getPlayer(), region, 50000);
+                                    } else {
+                                        e.getPlayer().sendMessage("§c必要金額は50000円ですが、所持金額が足りません。");
+                                        checkConfirm.put(e.getPlayer(), false);
+                                    }
+                                } else if (inside.matcher(region.getId()).matches()) { //内側区画の保護名と一致する場合
+                                    if (economy.getBalance(e.getPlayer()) >= 100000) {
+                                        regionPurchase(e.getPlayer(), region, 100000);
+                                    } else {
+                                        e.getPlayer().sendMessage("§c必要金額は100000円ですが、所持金額が足りません。");
+                                        checkConfirm.put(e.getPlayer(), false);
+                                    }
                                 }
                             }
                         }
                     }
                 }
+            }
+        }
+    }
+
+    private void regionPurchase(Player p, ProtectedRegion region, int cost) {
+        Economy economy = getEconomy();
+        economy.withdrawPlayer(p, cost);
+        DefaultDomain defaultDomain = region.getOwners();
+        defaultDomain.addPlayer(WorldGuardPlugin.inst().wrapPlayer(p));
+        region.setOwners(defaultDomain);
+        p.sendMessage("§6" + region.getId() + "§aを購入しました！");
+        checkConfirm.put(p, false);
+    }
+
+    @EventHandler
+    public void onMove(PlayerMoveEvent e) {
+        if (checkConfirm.get(e.getPlayer()) != null && checkConfirm.get(e.getPlayer())) {
+            if (clickedBlock.get(e.getPlayer()) != null) {
+                if (e.getPlayer().getLocation().distance(clickedBlock.get(e.getPlayer()).getLocation()) > 7) {
+                    checkConfirm.put(e.getPlayer(), false);
+                    e.getPlayer().sendMessage("§c購入をキャンセルしました。");
+                }
+            }
+        }
+    }
+
+    @EventHandler
+    public void onQuit(PlayerQuitEvent e) {
+        if (checkConfirm.get(e.getPlayer()) != null && checkConfirm.get(e.getPlayer())) {
+            checkConfirm.put(e.getPlayer(), false);
+        }
+    }
+
+    //全く関係ないやつ
+    @EventHandler
+    public void onRegionEnter(RegionEnteredEvent e) {
+        if (e.getPlayer().getWorld().getName().equals("resource") && e.getRegionName().equals("re_athletic")) {
+            if (e.getPlayer().getInventory().getChestplate() != null && e.getPlayer().getInventory().getChestplate().getType().equals(Material.ELYTRA)) {
+                ConsoleCommandSender console = Bukkit.getConsoleSender();
+                Bukkit.dispatchCommand(console, "warp " + e.getPlayer().getName() + " re_athletic");
+                e.getPlayer().sendMessage("エリトラを着用しているため入れません。");
+            } else {
+                if (e.getPlayer().getAllowFlight()) e.getPlayer().setAllowFlight(false);
+                if (e.getPlayer().hasPotionEffect(PotionEffectType.JUMP))
+                    e.getPlayer().removePotionEffect(PotionEffectType.JUMP);
+                e.getPlayer().sendMessage("アスレチックを開始しました。");
             }
         }
     }
